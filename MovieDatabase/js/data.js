f91f50f94f46e706979f44e4e37a3f5a31e47868
Jarrod Hermer <jarrodhermer@gmail.com>
2012-11-15 17:11:38 +0200

    Initial Commit
    Display hardcoded movie list using standard JavaScript and HTML

---
+++ MovieDatabase/js/data.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    "use strict";
+
+    var moviesArray = [
+        { title: "Skyfall", released: new Date("30 November 2012"), duration: 143 },
+        { title: "Wreck-It Ralph", released: new Date("2 November 2012"), duration: 101 },
+        { title: "Flight", released: new Date("2 November 2012"), duration: 138 },
+        { title: "Argo", released: new Date("12 October 2012"), duration: 120 },
+        { title: "Taken 2", released: new Date("12 October 2012"), duration: 92 }
+    ];
+
+
+    WinJS.Namespace.define("MovieData", {
+        movies: moviesArray
+    });
+})();
---
+++ MovieDatabase/pages/home/home.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/home/home.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            // TODO: Initialize the page here.
+            var movieList = document.getElementById("movieList");
+
+            MovieData.movies.forEach(function(movie) {
+                var item = document.createElement("li");
+                item.innerHTML = "Title: " + movie.title;
+                movieList.appendChild(item);
+            });
+        }
+    });
+})();
